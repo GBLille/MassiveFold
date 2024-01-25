26aa1b3c9dd201541ca49d7b0d9f3b3cc6746fa3
nessim.raouraoua.etu <nessim.raouraoua.etu@univ-lille.fr>
2024-01-25 17:34:35 +0100

    Restructuration of the architecture, spliting parallelization module from core AF modification, now named AFmassive

--- install.sh
+++ install.sh
@@ -17,30 +17,40 @@ elif [ ! -d $alphafold_databases ]; then
   exit 1
 fi
 
-mkdir -p $install_path/massivefold_runs/input
-mkdir $install_path/massivefold_runs/output
-mkdir $install_path/massivefold_runs/log
+runs=$install_path/massivefold_runs
+mkdir -p $runs/input
+mkdir $runs/output
+mkdir $runs/log
+mkdir $runs/scripts
 
-pipeline=$install_path/massivefold_runs/scripts/
+# scipts and header that are general to massivefold
+cp -r massivefold/parallelization/headers $runs/scripts
+cp massivefold/parallelization/*.py $runs/scripts
+
+# scripts and files for each pipeline (currently only AFmassive)
+pipeline=$runs/AFmassive_pipeline/
 mkdir $pipeline
-cp -r MF_scripts/parallelization/* $pipeline
+cp massivefold/parallelization/*.json $pipeline
+cp -r massivefold/parallelization/templates $pipeline
+cp massivefold/run_massivefold.sh $pipeline
+
 
 param_file=$pipeline/params.json
-
 params_with_paths=$(cat $param_file | python3 -c "
 import json
 import sys
 
 params = json.load(sys.stdin)
 
-params['MF_parallel']['run_massivefold'] = '$(realpath ./run_alphafold.py)'
-params['MF_parallel']['run_massivefold_plots'] = '$(realpath ./MF_scripts/plots/MF_plots.py)'
-params['MF_parallel']['data_dir'] = '$(echo $alphafold_databases)'
-params['MF_parallel']['jobfile_headers_dir'] = './headers/'
-params['MF_parallel']['jobfile_templates_dir'] = './templates/'
-params['MF_parallel']['output_dir'] = '../output/'
-params['MF_parallel']['logs_dir'] = '../log/'
-params['MF_parallel']['input_dir'] = '../input/'
+params['MF_parallel']['run_massivefold'] = '$(realpath ./AF/run_alphafold.py)'
+params['MF_parallel']['run_massivefold_plots'] = '$(realpath ./massivefold/massivefold_plots.py)'
+params['MF_parallel']['data_dir'] = '$(realpath $alphafold_databases)'
+params['MF_parallel']['jobfile_templates_dir'] = './templates'
+params['MF_parallel']['scripts_dir'] = '../scripts'
+params['MF_parallel']['jobfile_headers_dir'] = '../scripts/headers'
+params['MF_parallel']['output_dir'] = '../output'
+params['MF_parallel']['logs_dir'] = '../log'
+params['MF_parallel']['input_dir'] = '../input'
 with open('$param_file', 'w') as params_output:
   json.dump(params, params_output, indent=4)
 "
