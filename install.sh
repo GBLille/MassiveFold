0a688b31b2635f0329981dce268a28a0ac47427c
Guillaume Brysbaert <guillaume.brysbaert@univ-lille.fr>
2024-02-06 13:27:37 +0100

    Modified: version 1.1.0 added to install and yaml env

--- install.sh
+++ install.sh
@@ -25,9 +25,10 @@ if ! $host_is_jeanzay; then
   source $conda
   conda env create -f environment.yml
   conda activate massivefold-1.1.0
+  wget -O ${CONDA_PREFIX}/lib/python3.8/site-packages/alphafold/common/stereo_chemical_props.txt https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt
 
   # add run_AFmassive.py and massivefold_plots.py in path (python executables)
-  wget -O $CONDA_PREFIX/bin/run_AFmassive.py https://raw.githubusercontent.com/GBLille/AFmassive/devs_l/run_AFmassive.py
+  wget -O $CONDA_PREFIX/bin/run_AFmassive.py https://raw.githubusercontent.com/GBLille/AFmassive/v1.1.0/run_AFmassive.py
   chmod +x $CONDA_PREFIX/bin/run_AFmassive.py
 
   cp -r massivefold/plots $CONDA_PREFIX/bin/
