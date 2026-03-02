a69f7ab493324989d5789aded2de40d0b72ebf99
NessimRaou <nessim.raouraoua.etu@univ-lille.fr>
2026-03-02 13:51:21 +0100

    Add: PPI pipeline completion

--- src/massivefold/pipeline/ppi.py
+++ src/massivefold/pipeline/ppi.py
@@ -1,12 +1,12 @@
 """Implementation of `massivefold screen` pipeline."""
 
-import os
-import shutil
+from argparse import Namespace
 
-from .screen import run_screening_pipeline_internal
+from .screen import screening_pipeline
+from .run import run_pipeline
 from massivefold.parallelization.unifier import ppi_create_input
 
-def run_ppi_pipeline_internal(args, forwared_args, scheduler):
+def run_ppi_pipeline_internal(args, forwarded_args, scheduler):
   receptors_file = args.receptors
   ligands_file = args.ligands
   context_file = args.context
@@ -17,14 +17,34 @@ def run_ppi_pipeline_internal(args, forwared_args, scheduler):
   only_msas = args.only_msas
   wait_for_jobid = args.jobid
 
-  a = ppi_create_input(
+  ppi_inputs = ppi_create_input(
     receptors_file,
     ligands_file,
     context_file,
     parameters_file
   )
 
-  print(a)
+  ppi_sequences = ppi_inputs["ppi"].tolist()
+  base_args = {
+    "parameters": parameters_file,
+    "predictions_per_model": predictions_per_model,
+    "msas_precomputed": msas_precomputed,
+    "only_msas": only_msas,
+    "wait_for_jobid": wait_for_jobid
+  }
+  for ppi in ppi_sequences:
+    args_ppi = base_args.copy()
+    args_ppi["sequence"] = ppi
+    if context_file:
+      args_ppi["ligands"] = context_file
+      args = Namespace(**args_ppi)
+      screening_pipeline(args, forwarded_args, scheduler)
+    else:
+      args_ppi["run"] = "PPI"
+      args = Namespace(**args_ppi)
+      run_pipeline(args, forwarded_args, scheduler)
+
+  return 0
 
 def ppi_pipeline(args, forwarded_args, scheduler):
   try:
